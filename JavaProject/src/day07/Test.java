d3f624968f4609b9478804d482dea6d02fd40575
sds2317884 <lee2317884@naver.com>
2022-09-27 14:24:45 +0900

    day07

---
+++ JavaProject/src/day07/Test.java
@@ -0,0 +1,12 @@
+package day07;
+
+
+import javax.swing.JFrame;
+public class Test {
+	public static void main(String[] args) {
+		JFrame f = new JFrame();
+		f.setSize(400, 400);
+		f.setVisible(true);
+	}
+		
+}
